7a8e60f06b301a7243f388c71f78a8b91049f1fe
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2023-10-07 01:26:32 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 18.95 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        
+        // 4개의 사분면 중 1개만 구한뒤 4를 곱한다.
+        for( int i = 1; i <= r2 ; i++){
+            double y2 = Math.sqrt(Math.pow(r2,2) - Math.pow(i,2));
+            double y1 = Math.sqrt(Math.pow(r1,2) - Math.pow(i,2));
+            answer += ( (long)y2 - (long)Math.ceil(y1) + 1);
+        }
+        answer *= 4;
+        
+        return answer;
+    }
+}
