2661194555cea6d145603e2990429ad07588ce64
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2023-11-20 16:09:24 +0900

    [level unrated] Title: 외계어 사전, Time: 0.40 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.java
@@ -1,12 +1,18 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
+        int count = 0;
         for(int i = 0; i<dic.length;i++){
             for(int j = 0; j<spell.length;j++){
-                dic[i] = dic[i].replaceFirst(spell[j]," ");
-            }
-            if(dic[i].length() == spell.length && dic[i].replace(" ","").equals(""))
-                return 1;
+                dic[i] = dic[i].replaceFirst(spell[j],"");
+                if(dic[i].equals("") && j!=spell.length-1)
+                    dic[i] = "1";
+            }         
+            if(dic[i].equals(""))
+                count++;
         }
+        if(count == 1)
+            return 1;
+        
         return 2;
     }
 }
