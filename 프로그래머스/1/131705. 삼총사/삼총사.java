89db223290dc9ba6b6ff165f7bc058c57d884168
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2023-12-23 22:42:33 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int dfs(int start,int len,int count,int []number,int sum,int[] temp){
+        if(count==2) {
+            if (sum == 0) {
+                temp[0]+=1;
+                return 1;
+            }
+            return 0;
+        }
+        for (int i = start; i < len; i++) {
+            int sum_dfs = sum;
+            sum_dfs += number[i];
+            dfs(i + 1, len, count + 1, number, sum_dfs,temp);
+        }
+        return 0;
+    }
+    public int solution(int[] number) {
+        int [] temp = {0};
+        int answer=0,len =number.length,result=0;
+
+        for (int i = 0; i < len; i++) 
+            dfs(i+1,len,0,number,number[i],temp);
+
+        return temp[0];
+    }
+}
