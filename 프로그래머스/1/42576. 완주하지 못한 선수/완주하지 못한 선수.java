759549df07badb235c13dd0e8061e6430bfc6991
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-02-05 19:53:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 116.74 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,20 +1,24 @@
-import java.util.*; 
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        LinkedList<String> retire = new LinkedList<>(Arrays.asList(participant));
-        retire.sort(Comparator.naturalOrder());
-        Arrays.sort(completion);
-        for (int i = 0 ; i < completion.length ; i++) {
-            if (retire.get(0).equals(completion[i])) {
-                retire.remove(0);
-            }
-            else if (retire.get(1).equals(completion[i])) {
-                retire.remove(1);
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return key;
             }
         }
-        answer = retire.get(0);
-        return answer;
+
+        return "";
     }
 }
