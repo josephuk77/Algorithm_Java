fd35769e5a4905b12d3d3aef9f46d34b483a5ed0
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-03-10 19:47:27 +0900

    [Bronze III] Title: 소금 폭탄, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -3,29 +3,52 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] start = sc.nextLine().split(":");
-        String[] end = sc.nextLine().split(":");
 
-        int startHour = Integer.parseInt(start[0]);
-        int startMinute = Integer.parseInt(start[1]);
-        int startSecond = Integer.parseInt(start[2]);
-        int startTotalSecond = startHour * 3600 + startMinute * 60 + startSecond;
+        String[] arr = sc.nextLine().split(":");
 
-        int endHour = Integer.parseInt(end[0]);
-        int endMinute = Integer.parseInt(end[1]);
-        int endSecond = Integer.parseInt(end[2]);
-        int endTotalSecond = endHour * 3600 + endMinute * 60 + endSecond;
+        int[] input = new int[arr.length];
 
-        int newTotalSecond = endTotalSecond - startTotalSecond;
-        if (newTotalSecond <= 0) {
-            newTotalSecond += 24 * 3600;
+        for(int i = 0; i < arr.length; i++){
+            input[i] = Integer.parseInt(arr[i]);
         }
+        String[] arr2 = sc.nextLine().split(":");
 
-        int newHour = newTotalSecond / 3600;
-        int newMinute = (newTotalSecond % 3600) / 60;
-        int newSecond = newTotalSecond % 60;
+        int[] input2 = new int[arr.length];
 
-        String ans = String.format("%02d:%02d:%02d", newHour, newMinute, newSecond);
-        System.out.println(ans);
+        for(int i = 0; i < arr2.length; i++){
+            input2[i] = Integer.parseInt(arr2[i]);
+        }
+        
+        int time[] = new int[3];
+        
+        for(int i = 0; i < 3; i++) {
+        	if(input[i] > input2[i] && i == 0) {
+        		time[i] = input2[i] - input[i] + 24;
+        	}else if(input[i] > input2[i]) {
+        		time[i] = input2[i] - input[i] + 60;
+        		if(time[i - 1] == 0 && i == 1) {
+        			time[i-1] = 23;
+        		}
+        		else if(time[i - 1] == 0 && i == 2) {
+        			time[i-1] = 59;
+        			if(time[i-2] == 0)
+        				time[i-2] = 23;
+        			else
+        				time[i-2]--;
+        		}else {
+        			time[i -1]--;
+        		}
+        		
+        	}else {
+        	
+        		time[i] = input2[i] - input[i];
+        	}
+        }
+        if(time[0] == 0 && time[1] == 0 && time[2] == 0)
+        	System.out.print("24:00:00");
+        else
+        	System.out.printf("%02d:%02d:%02d",time[0],time[1],time[2]);
+        
+        
     }
 }
