86fe2c0e6d094d32daa9f407f512e52bbdfe51b6
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-03-30 12:12:00 +0900

    [Silver II] Title: 좌표 압축, Time: 3100 ms, Memory: 375084 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+        int[] sorted = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+            sorted[i] = arr[i];
+        }
+
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (!map.containsKey(sorted[i])) {
+                map.put(sorted[i], idx++);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
