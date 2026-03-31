482869d6ce8397de9897f6cd6d2986c6c5b0da76
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-03-31 11:38:16 +0900

    [level 0] Title: l로 만들기, Time: 5.46 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.charAt(i) < 'l')
+                myString = myString.replace(myString.charAt(i), 'l');
+        }
+        return myString;
+    }
+}
