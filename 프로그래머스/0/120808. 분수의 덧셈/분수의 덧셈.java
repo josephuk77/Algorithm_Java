140fc7f72e8299bba346ce6d58c5c33be614bcfe
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-04-01 09:17:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 12.78 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        
+        int numer = numer1*denom2 + numer2*denom1;
+        int denom = denom1*denom2;
+        for(int i = 1; i<1000000; i++){
+            if(numer%i==0 && denom%i==0){
+                numer /= i;
+                denom /= i;
+                if(i!=1)
+                    i--;
+            }
+        }
+        int[] answer = {numer, denom};
+        return answer;
+    }
+}
