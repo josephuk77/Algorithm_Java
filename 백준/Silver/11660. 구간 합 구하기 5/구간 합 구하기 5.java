120787b9e58bc2f166c5feb3318fcf1954011873
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-04-03 10:45:01 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2660 ms, Memory: 319184 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,40 +1,35 @@
-import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] nums = new int[N + 1][N + 1];
 
-        int[] nums = new int[N * N + 1];
-        int sum = 0;
-
-        for (int i = 1; i <= N * N; i++) {
-            while (st == null || !st.hasMoreTokens()) {
-                st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                nums[i][j] = sc.nextInt();
             }
-            sum += Integer.parseInt(st.nextToken());
-            nums[i] = sum;
+        }
+
+        int[][] sum = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sum[i][j] = nums[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
         }
 
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
 
-            int result = 0;
-            for (int j = x1; j <= x2; j++) {
-                result += nums[N * (j - 1) + y2] - nums[N * (j - 1) + y1 - 1];
-            }
-            sb.append(result).append('\n');
+            int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
+            System.out.println(result);
         }
-
-        System.out.print(sb);
     }
 }
