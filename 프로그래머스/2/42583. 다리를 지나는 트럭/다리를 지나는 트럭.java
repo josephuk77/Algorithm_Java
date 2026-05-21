2089342432c6902420958ae3fe27c6dc191a0b0b
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-05-21 14:12:58 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 25.31 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,14 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Bridge bridge = new Bridge(bridge_length, weight);
-        for(int i = 0; i < truck_weights.length; i++){
+
+        for (int i = 0; i < truck_weights.length; i++) {
             boolean add = bridge.addTruck(new Truck(truck_weights[i]));
-            if(!add) {
+
+            if (!add) {
                 i--;
             }
+
             bridge.moveTrucks();
         }
+
         bridge.moveTrucksEnd();
+
         return bridge.count;
     }
 }
@@ -20,70 +25,67 @@ class Bridge {
     int weight;
     int total_weight;
     int count;
-    List<Truck> trucks;
-    
+    Queue<Truck> trucks;
+
     public Bridge(int length, int weight) {
         this.length = length;
         this.weight = weight;
         this.total_weight = 0;
         this.count = 1;
-        this.trucks = new ArrayList<>();
+        this.trucks = new LinkedList<>();
     }
-    
+
     public boolean addTruck(Truck truck) {
-        if(weight >= truck.weight+total_weight){
+        if (weight >= truck.weight + total_weight) {
             this.total_weight += truck.weight;
-            trucks.add(truck);
-            
+            trucks.offer(truck);
             return true;
         }
+
         return false;
     }
-    
+
     public void moveTrucks() {
-        if(!trucks.isEmpty()) {
-            for(Truck truck : trucks) {
+        if (!trucks.isEmpty()) {
+            for (Truck truck : trucks) {
                 truck.moveTruck();
             }
+
             count++;
-            for(int i = trucks.size() - 1; i >= 0; i--){
-                if (trucks.get(i).location == this.length) {
-                    this.total_weight -= trucks.get(i).weight;
-                    trucks.remove(i);
-                    
-                }
+
+            if (trucks.peek().location == this.length) {
+                Truck outTruck = trucks.poll();
+                total_weight -= outTruck.weight;
             }
         }
     }
-    
+
     public void moveTrucksEnd() {
-        while(!trucks.isEmpty()) {
-            for(Truck truck : trucks) {
+        while (!trucks.isEmpty()) {
+            for (Truck truck : trucks) {
                 truck.moveTruck();
             }
+
             count++;
-            for(int i = trucks.size() - 1; i >= 0; i--){
-                if (trucks.get(i).location == this.length) {
-                    trucks.remove(i);
-                }
+
+            if (trucks.peek().location == this.length) {
+                Truck outTruck = trucks.poll();
+                total_weight -= outTruck.weight;
             }
         }
     }
-    
-    
 }
+
 class Truck {
     int weight;
     int location;
 
-    
     public Truck(int weight) {
         this.weight = weight;
         this.location = 0;
     }
-    
+
     public void moveTruck() {
         this.location++;
     }
-    
 }
