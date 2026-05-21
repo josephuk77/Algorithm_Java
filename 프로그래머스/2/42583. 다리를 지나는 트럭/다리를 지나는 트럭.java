ba7533dfcf5925e3c2b243a6da896af75df67ba2
josephuk77 <69853298+josephuk77@users.noreply.github.com>
2026-05-21 10:57:55 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 32.00 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Bridge bridge = new Bridge(bridge_length, weight);
+        for(int i = 0; i < truck_weights.length; i++){
+            boolean add = bridge.addTruck(new Truck(truck_weights[i]));
+            if(!add) {
+                i--;
+            }
+            bridge.moveTrucks();
+        }
+        bridge.moveTrucksEnd();
+        return bridge.count+1;
+    }
+}
+
+class Bridge {
+    int length;
+    int weight;
+    int total_weight;
+    int count;
+    List<Truck> trucks;
+    
+    public Bridge(int length, int weight) {
+        this.length = length;
+        this.weight = weight;
+        this.total_weight = 0;
+        this.count = 0;
+        this.trucks = new ArrayList<>();
+    }
+    
+    public boolean addTruck(Truck truck) {
+        if(weight >= truck.weight+total_weight){
+            this.total_weight += truck.weight;
+            trucks.add(truck);
+            
+            return true;
+        }
+        return false;
+    }
+    
+    public void moveTrucks() {
+        if(!trucks.isEmpty()) {
+            for(Truck truck : trucks) {
+                truck.moveTruck();
+            }
+            count++;
+            for(int i = trucks.size() - 1; i >= 0; i--){
+                if (trucks.get(i).location == this.length) {
+                    this.total_weight -= trucks.get(i).weight;
+                    trucks.remove(i);
+                    
+                }
+            }
+        }
+    }
+    
+    public void moveTrucksEnd() {
+        while(!trucks.isEmpty()) {
+            for(Truck truck : trucks) {
+                truck.moveTruck();
+            }
+            count++;
+            for(int i = trucks.size() - 1; i >= 0; i--){
+                if (trucks.get(i).location == this.length) {
+                    trucks.remove(i);
+                }
+            }
+        }
+    }
+    
+    
+}
+class Truck {
+    int weight;
+    int location;
+
+    
+    public Truck(int weight) {
+        this.weight = weight;
+        this.location = 0;
+    }
+    
+    public void moveTruck() {
+        this.location++;
+    }
+    
+}
